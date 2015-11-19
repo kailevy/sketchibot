f6e8553a23b1eac4eb3bf2ffaa337a7b4c9236d5
Alex <acrease77@gmail.com>
2015-11-19 17:04:37 -0500

    adding another image

--- scripts/screen_drawing.py
+++ scripts/screen_drawing.py
@@ -20,7 +20,8 @@ class PathDrawing():
         self.pagey = float(y)
         self.patch_size = (self.pagey*self.scale,self.pagex*self.scale)
         # we will draw our patch on im
-        self.im = 255*np.ones(self.patch_size,dtype=np.uint8)
+        self.im1 = 255*np.ones(self.patch_size,dtype=np.uint8)
+        self.im2 = 255*np.ones(self.patch_size,dtype=np.uint8)
 
     def mouse_event(self,event,x,y,flag,dc):
         """ handle mouse events, basically lets you sketch by clicking in the left pane """
@@ -37,32 +38,35 @@ class PathDrawing():
             #strokes.append('NNNEEEEEEEEEWWWWW STROOKKEEE')
         elif event == cv2.EVENT_MOUSEMOVE and self.drawing:
             # draw a line between the last mouse position and the current one
-            cv2.line(self.im,(int(x),int(y)),(int(last_x),int(last_y)),0,2)
+            cv2.line(self.im1,(int(x),int(y)),(int(last_x),int(last_y)),0,2)
             last_x = x
             last_y = y
             self.stroke.append([x,y])
 
     def draw_strokes(self):
-        cv2.namedWindow("mywin")
-        cv2.setMouseCallback("mywin", self.mouse_event)
+        cv2.namedWindow("mywin1")
+        cv2.namedWindow("mywin2")
+        cv2.setMouseCallback("mywin1", self.mouse_event)
 
         print "Draw on the canvas by clicking and holding the mouse (move slowly)"
         print "Reset the sketch by pressing the spacebar"
 
         while not rospy.is_shutdown():
-            cv2.imshow("mywin", self.im)
+            cv2.imshow("mywin1", self.im1)
+            cv2.imshow("mywin2", self.im2)
             key = cv2.waitKey(25)
             if key != -1 and chr(key) == ' ':
                 # if you hit space bar, you should reset the sketch on the left
-                self.im =  255*np.ones(self.patch_size,dtype=np.uint8)
+                #self.im1 =  255*np.ones(self.patch_size,dtype=np.uint8)
                 print self.strokes
                 self.scale_strokes()
                 print 'Scaled:'
                 print self.strokes
                 print 'Filtered:'
-                self.colinear_points()
                 self.point_distance_filtering()
+                self.colinear_points()
                 print self.strokes
+                self.plot_strokes()
                 self.strokes = []
 
     def scale_strokes(self):
@@ -78,7 +82,7 @@ class PathDrawing():
                 if i<len(path)-1:
                     if i>1:
                         distance = sqrt((path[i][0]-path[i+1][0])**2 + (path[i][1]-path[i+1][1])**2)
-                        if distance < .25:
+                        if distance < .5:
                             path.pop(i+1)
 
     def colinear_points(self):
@@ -90,7 +94,7 @@ class PathDrawing():
                         radangle2 = atan2(path[i+2][1]-path[i+1][1],path[i+2][0]-path[i+1][0])
                         angle1 = radangle1*180/pi
                         angle2 = radangle2*180/pi
-                        if abs(angle2-angle1) < 45:
+                        if abs(angle2-angle1) < 10:
                             path.pop(i+1)
 
     def add_heading(self):
@@ -101,6 +105,12 @@ class PathDrawing():
                     path[i].append(heading)
                 else:
                     path[i].append(heading)#appends previous heading if no extra path
+
+    def plot_strokes(self):
+        for path in self.strokes:
+            for i in range(len(path)):
+                if i<len(path)-1:
+                    cv2.arrowedLine(self.im2, (int(path[i][0]*self.scale), int(path[i][1]*self.scale)), (int(path[i+1][0]*self.scale), int(path[i+1][1]*self.scale)), (0, 0, 255), thickness=1)
 
 if __name__ == '__main__':
     drawing = PathDrawing()
